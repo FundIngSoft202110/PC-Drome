1be215e0537e567e12b017be395a28fb04b12888
vesgae <vesga.e@javeriana.edu.co>
2021-04-02 14:05:19 -0500

    initial, components

--- PCDROME/PCDROME/src/app/app.module.ts
+++ PCDROME/PCDROME/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './presentation/home/home.component';
+import { LoginFloatComponent } from './presentation/login-float/login-float.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginFloatComponent
   ],
   imports: [
     BrowserModule,
