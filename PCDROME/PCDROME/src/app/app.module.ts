ddad543d5b310e204e91b5f64875e220cb242677
vesgae <vesga.e@javeriana.edu.co>
2021-04-13 22:43:53 -0500

    Componentes, material module y varios.

--- PCDROME/PCDROME/src/app/app.module.ts
+++ PCDROME/PCDROME/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MaterialModule} from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './presentation/home/home.component';
 import { LoginFloatComponent } from './presentation/login-float/login-float.component';
+import { ArmatupcComponent } from './presentation/armatupc/armatupc.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginFloatComponent
+    LoginFloatComponent,
+    ArmatupcComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
